7c343d9998c34d54f0dd4194b1d0a4221118cbcf
Vihaan Sondhi <vihaansondhi07@gmail.com>
2024-05-12 17:24:35 -0400

    package

--- app/page.tsx
+++ app/page.tsx
@@ -24,17 +24,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/coltsneckhighschool.svg"
-          alt="Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      <p className="text-3xl font-bold"> CNHS Girls Soccer</p>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://cnhs-girls-soccer.vercel.app/stats"
@@ -49,7 +39,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            View current and previous statistics for the CNHS Girls Soccer Team.
+            View current and previous statistics.
           </p>
         </a>
 
@@ -66,7 +56,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            View the schedule for the CNHS Girls Soccer Time.
+            View the schedule.
           </p>
         </a>
 
@@ -83,7 +73,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            General information about the CNHS Girls Soccer Team.
+            General information.
           </p>
         </a>
 
@@ -103,6 +93,9 @@ export default function Home() {
             For players: explore the latest posts from Coach Hein. <br />
           </p>
         </a>
+          <div className="fixed bottom-0 right-0 mb-4 mr-4 text-sm text-gray-500">
+            <span className="font-bold">Email:</span>  khein@frhsd.com 
+          </div>
       </div>
     </main>
   );
--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,3 +1,31 @@
 export default function Page() {
-    return <p>Stats Page</p>;
-  }
+    return (
+        
+        <div className="p-4">
+            <p className="text-3xl font-bold mb-10">Stats</p>
+
+            <table className="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 w-1/3">Year</th>
+                        <th className="px-4 py-2 w-2/3">Wins?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border px-4 py-2 w-1/3">
+                            <div className="w-full">2024</div>
+                        </td>
+                        <td className="border px-4 py-2 w-2/3">1,000,000</td>
+                    </tr>
+                    <tr>
+                        <td className="border px-4 py-2 w-1/3">
+                            <div className="w-full">2023</div>
+                        </td>
+                        <td className="border px-4 py-2 w-2/3">1,000,000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
