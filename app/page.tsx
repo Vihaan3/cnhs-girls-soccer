2ed0582b9d5e057bb387788c3e9e771ea2e05f67
Vihaan Sondhi <vihaansondhi07@gmail.com>
2024-05-12 16:29:30 -0400

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         </a>
 
         <a
-          href= ""
+          href= "https://cnhs-girls-soccer.vercel.app/schedule"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -71,7 +71,7 @@ export default function Home() {
         </a>
 
         <a
-          href= ""
+          href= "https://cnhs-girls-soccer.vercel.app/gen"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -88,7 +88,7 @@ export default function Home() {
         </a>
 
         <a
-          href=""
+          href="https://cnhs-girls-soccer.vercel.app/players"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
